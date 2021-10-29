59777af277d64dee5fb93fa4c675bd4d8974db27
Nimmi-13 <54712881+Nimmi-13@users.noreply.github.com>
2021-10-29 20:19:04 +0530

    Update todo.js

--- Desktop/Nimmi/assignment/todoapp/src/todo.js
+++ Desktop/Nimmi/assignment/todoapp/src/todo.js
@@ -1,31 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
+import{ Component } from 'react';
+   
+    class Todo extends Component {
+      constructor() {
+        super();
+        this.state = {
+         items: [],
+        };
+       this.handleChange = this.handleChange.bind(this);
+      }
+    
+      componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+          .then(res => res.json())
+          .then(items => this.setState({ items }));
+          };
+       
+          handleChange = (items) => {
+            this.setState({ items: !this.state.items.completed });
+          };
 
-function Todo(){
-  const [data, setData] = useState(null)
-  const fetchURL = "https://jsonplaceholder.typicode.com/todos"
-  const getData = () =>
-    fetch(`${fetchURL}`)
-      .then((res) => res.json())
-
-  useEffect(() => {
-    getData().then((data) => setData(data))
-  }, [])
-
- 
- 
-    return (
-    <div>
-      <h2 className="head"> Todo List </h2>
-      {data?.map((item) => 
-        <ul>
-          <li>{item.id}</li>
-          <li>{item.title}</li>
-          <li><input type="checkbox" checked={item.completed} />
-            </li>
-        </ul>
-      )}
-    </div>
-  )
-}
+         
+         
+    
+      render() {
+        const { items } = this.state;
+             return (
+            <div>
+              <h2 className="head"> Todo List </h2>
+              {items.map(item => (
+               <ul>
+               <li>{item.id}</li>
+               <li>{item.title}</li>
+               <li><input type="checkbox" checked={item.completed} onChange={this.handleChange}/>
+                 </li>
+             </ul>
+              ))}
+            </div>
+          );
+        }
+      }
+    
 
 export default Todo;
